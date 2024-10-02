a3689ecd483058d8cd3279d7dbf12e575fa5be2d
MatthewDelego <129423200+MatthewDelego@users.noreply.github.com>
2024-10-02 17:03:25 -0400

    Update Contract.sol
    revert changes to original contract

--- Contract.sol
+++ Contract.sol
@@ -1,8 +1,10 @@
+Contract.sol
+
 pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
 
-    event NewZombie(uint zombieId, string name, uint dna);
+    // declare our event here
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -14,14 +16,9 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    mapping (uint => address) public zombieToOwner;
-    mapping (address => uint) ownerZombieCount;
-
-    function _createZombie(string memory _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
-        zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
-        emit NewZombie(id, _name, _dna);
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+        // and fire it here
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
@@ -30,9 +27,7 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
-        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
-        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
